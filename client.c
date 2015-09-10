e2dac0705a05b5309c63eb4927834d13f350d83e
Gyumin Sim <sim0629@gmail.com>
2015-09-10 18:17:34 +0900

    Parse command-line argument

--- client.c
+++ client.c
@@ -7,18 +7,27 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <signal.h>
+#include <assert.h>
 
 #define BUFFER_SIZE 1000    // same as packet size
 
 void handler();
 timer_t set_timer(long long);
 
+// Command-line arguments
+static char *hostname;
+static int port;
+static int window_size;
+static int ack_delay_ms;
+
 static void check_arguments(int argc, char **argv);
+static void parse_arguments(int argc, char **argv);
 static void set_handler_for_timers();
 
 int main (int argc, char **argv) {
 
     check_arguments(argc, argv);
+    parse_arguments(argc, argv);
 
     set_handler_for_timers();
 
@@ -46,6 +55,22 @@ static void check_arguments(int argc, char **argv) {
         printf("\tMandatory Arguments:\n");
         printf("\t\t-w\t\tsize of the window used at server\n");
         printf("\t\t-d\t\tACK delay in msec\n");
+        exit(1);
+    }
+}
+
+static void parse_arguments(int argc, char **argv) {
+    assert(argc == 7);
+    hostname = argv[1];
+    port = atoi(argv[2]);
+    if(strcmp(argv[3], "-w") == 0 && strcmp(argv[5], "-d") == 0) {
+        window_size = atoi(argv[4]);
+        ack_delay_ms = atoi(argv[6]);
+    }else if(strcmp(argv[3], "-d") == 0 && strcmp(argv[5], "-w") == 0) {
+        ack_delay_ms = atoi(argv[4]);
+        window_size = atoi(argv[6]);
+    }else {
+        fprintf(stderr, "Error: parsing arguments\n");
         exit(1);
     }
 }
